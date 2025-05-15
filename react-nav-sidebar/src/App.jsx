30a9a16168bd49c87b7bc43888e2047fbed28432
MattiasKopparberg <mkopparberg@hotmail.com>
2025-05-15 11:52:06 +0200

    Added basic file structure and REACT Vite

---
+++ react-nav-sidebar/src/App.jsx
@@ -0,0 +1,15 @@
+import Sidebar from './Components/Sidebar'
+import Navbar from './Components/Navbar'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+  <Navbar />
+  <Sidebar />
+    </>
+  )
+}
+
+export default App
---
+++ react-nav-sidebar/src/Components/Navbar.jsx
@@ -0,0 +1,13 @@
+export default function Navbar() {
+    return(
+        <div className="navbar">
+            <div className="navContent">
+                <ul className="navList">
+                    <li><a>Home</a></li>
+                    <li><a>About</a></li>
+                    <li><a>Contact Us</a></li>
+                </ul>
+            </div>
+        </div>
+    )
+}
